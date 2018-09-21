29aa8e18d0f2178340949b6518a6c62aa328ab8e
RUKAIYIN <xintingapple@gmail.com>
2018-09-21 12:17:28 +0200

    Initial commit

---
+++ src/ai2018/group9/Group9_AS.java
@@ -0,0 +1,85 @@
+package ai2018.group9;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import genius.core.boaframework.AcceptanceStrategy;
+import genius.core.boaframework.Actions;
+import genius.core.boaframework.BOAparameter;
+import genius.core.boaframework.NegotiationSession;
+import genius.core.boaframework.OfferingStrategy;
+import genius.core.boaframework.OpponentModel;
+
+public class Group9_AS extends AcceptanceStrategy {
+
+	private double a;
+	private double b;
+	private List<Double> oppoBidHistory;
+
+	/**
+	 * Empty constructor for the BOA framework.
+	 */
+	public Group9_AS() {
+	}
+
+	public Group9_AS(NegotiationSession negoSession, OfferingStrategy strat, double alpha, double beta) {
+		this.negotiationSession = negoSession;
+		this.offeringStrategy = strat;
+		this.a = alpha;
+		this.b = beta;
+		this.oppoBidHistory = new ArrayList<Double>();
+	}
+
+	@Override
+	public void init(NegotiationSession negoSession, OfferingStrategy strat, OpponentModel opponentModel,
+			Map<String, Double> parameters) throws Exception {
+		this.negotiationSession = negoSession;
+		this.offeringStrategy = strat;
+
+		if (parameters.get("a") != null || parameters.get("b") != null) {
+			a = parameters.get("a");
+			b = parameters.get("b");
+		} else {
+			a = 1;
+			b = 0;
+		}
+	}
+
+	@Override
+	public String printParameters() {
+		String str = "[a: " + a + " b: " + b + "]";
+		return str;
+	}
+
+	@Override
+	public Actions determineAcceptability() {
+		double nextMyBidUtil = offeringStrategy.getNextBid().getMyUndiscountedUtil();
+		double lastOpponentBidUtil = negotiationSession.getOpponentBidHistory().getLastBidDetails()
+				.getMyUndiscountedUtil();
+
+		if (a * lastOpponentBidUtil + b >= nextMyBidUtil) {
+			return Actions.Accept;
+		}
+		return Actions.Reject;
+	}
+
+	@Override
+	public Set<BOAparameter> getParameterSpec() {
+
+		Set<BOAparameter> set = new HashSet<BOAparameter>();
+		set.add(new BOAparameter("a", 1.0,
+				"Accept when the opponent's utility * a + b is greater than the utility of our current bid"));
+		set.add(new BOAparameter("b", 0.0,
+				"Accept when the opponent's utility * a + b is greater than the utility of our current bid"));
+
+		return set;
+	}
+
+	@Override
+	public String getName() {
+		return "Group9_AS example";
+	}
+}
