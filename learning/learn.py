21eea7af467513d32e971fba6d2a51c312277726
Tómas Þorbjarnarson <tomas@dohop.com>
2018-12-15 17:38:07 +0100

    Calculate all moves for sessions in /training_logs

--- learning/learn.py
+++ learning/learn.py
@@ -1,17 +1,99 @@
 import json
-import sys
+import os
+from pprint import pprint
 
-def read_json_file(file):
-  raw_json=open(file).read()
-  data = json.loads(raw_json)
-  return data
+training_dir = '../training_logs'
+parties = {'conceder': [], 'random': [], 'hardheaded': [], 'tft': []}
+thresh = 0.005
 
+def init_agent(name, prefs):
+  return {
+    'name': name,
+    'prefs': prefs,
+    'moves': {
+      'fortunate': 0,
+      'selfish': 0,
+      'concession': 0,
+      'unfortunate': 0,
+      'nice': 0,
+      'silent': 0
+    },
+    'bids': [],
+  }
 
+def read_json_files():
+  sessions = []
+  files = os.listdir(training_dir)
+  for file in files:
+    raw_json=open(training_dir + '/' + file).read()
+    sessions.append({'id': file.replace('.json', ''), 'data': json.loads(raw_json)})
+  return sessions
+
+def update_moves(agent, opponent):
+  last_u1 = get_bid_util(agent['prefs'], agent['bids'][0])
+  last_u2 = get_bid_util(opponent['prefs'], opponent['bids'][0])
+  for bid in agent['bids'][1:]:
+    u1 = get_bid_util(agent['prefs'], bid)
+    u2 = get_bid_util(opponent['prefs'], bid)
+    u1_diff = u1 - last_u1
+    u2_diff = u2 - last_u2
+    if abs(u1_diff) < thresh:
+      if abs(u2_diff) < thresh:
+        agent['moves']['silent'] += 1
+      elif u2_diff > thresh:
+        agent['moves']['nice'] += 1
+    elif u1_diff >= thresh:
+      if u2_diff >= 0:
+        agent['moves']['fortunate'] += 1
+      else:
+        agent['moves']['selfish'] += 1
+    else:
+      if u2_diff >= 0:
+        agent['moves']['concession'] += 1
+      else:
+        agent['moves']['unfortunate'] += 1
+
+    last_u1 = u1
+    last_u2 = u2
+
+def calc_session(session):
+  session_id = ''.join([i for i in session['id'] if not i.isdigit()])
+  session_data = session['data']
+  agent1_name, agent2_name = session_id.split('_')
+
+  print('')
+  print('Session: ' + session_id)
+
+  agent1 = init_agent(agent1_name, session_data['Utility1'])
+  agent2 = init_agent(agent2_name, session_data['Utility2'])
+  agent1['bids'] = [x['agent1'] for x in session_data['bids'] if 'agent1' in x]
+  agent2['bids'] = [x['agent2'] for x in session_data['bids'] if 'agent2' in x]
+
+  update_moves(agent1, agent2)
+  update_moves(agent2, agent1)
+
+  del agent1['bids']
+  del agent1['prefs']
+  del agent2['bids']
+  del agent2['prefs']
+
+  pprint(agent1)
+  pprint(agent2)
+
+def get_bid_util(agent_util, bid):
+  u = 0
+  fruit, juice, topping1, topping2 = bid.split(',')
+  u += agent_util['Fruit'][str(fruit)]*agent_util['Fruit']['weight']
+  u += agent_util['Juice'][str(juice)]*agent_util['Juice']['weight']
+  u += agent_util['Topping1'][str(topping1)]*agent_util['Topping1']['weight']
+  u += agent_util['Topping2'][str(topping2)]*agent_util['Topping2']['weight']
+  return u
 
 '''
-  Usage: python learn.py ../training_logs/conceder_conceder.json
+  Usage: python learn.py
   Currently prints out the json file
 '''
 if __name__ == "__main__":
-    data = read_json_file(sys.argv[1])
-    print(data)
+    sessions = read_json_files()
+    for session in sessions:
+      calc_session(session)
