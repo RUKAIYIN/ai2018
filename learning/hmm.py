1d7a0ebb7cb8453e734bcaba21acd7080d1e64af
Nathan Buskulic <nathan.buskulic@outlook.fr>
2019-01-05 16:09:17 +0100

    cut the program in 2 and change the output format

--- learning/hmm.py
+++ learning/hmm.py
@@ -1,4 +1,4 @@
-# import sys
+import sys
 import numpy as np
 import json
 import os
@@ -170,128 +170,17 @@ class Hmm(AbstractHmm):
 
 
 
-            
-    ###################### LEARNING ################################
 
-            
-    def learningModel(self,dirLogs):
-        ''' Learn a model through training logs with a simple counting method and update the observation matrix '''
-
-        training_dir = dirLogs
-        parties = {'conceder': [], 'random': [], 'hardheaded': [], 'tft': []}
-        thresh = 0.005
-
-        def init_moves():
-            return {
-                'fortunate': 0,
-                'selfish': 0,
-                'concession': 0,
-                'unfortunate': 0,
-                'nice': 0,
-                'silent': 0
-            }
-
-        def init_agent(name, prefs):
-            return {
-                'name': name,
-                'prefs': prefs,
-                'moves': init_moves(),
-                'bids': [],
-            }
-
-        def read_json_files():
-            sessions = []
-            files = os.listdir(training_dir)
-            for file in files:
-                raw_json = open(training_dir + '/' + file).read()
-                sessions.append({'id': file.replace('.json', ''), 'data': json.loads(raw_json)})
-            return sessions
-
-        def update_moves(agent, opponent):
-            last_u1 = get_bid_util(agent['prefs'], agent['bids'][0])
-            last_u2 = get_bid_util(opponent['prefs'], opponent['bids'][0])
-            for bid in agent['bids'][1:]:
-                u1 = get_bid_util(agent['prefs'], bid)
-                u2 = get_bid_util(opponent['prefs'], bid)
-                u1_diff = u1 - last_u1
-                u2_diff = u2 - last_u2
-                if abs(u1_diff) < thresh:
-                    if abs(u2_diff) < thresh:
-                        agent['moves']['silent'] += 1
-                    elif u2_diff > 0:
-                        agent['moves']['nice'] += 1
-                    else:
-                        if u1_diff >= 0:
-                            agent['moves']['selfish'] += 1
-                        else:
-                            agent['moves']['unfortunate'] += 1
-                elif u1_diff >= thresh:
-                    if u2_diff >= 0:
-                        agent['moves']['fortunate'] += 1
-                    else:
-                        agent['moves']['selfish'] += 1
-                else:
-                    if u2_diff >= 0:
-                        agent['moves']['concession'] += 1
-                    else:
-                        agent['moves']['unfortunate'] += 1
-                last_u1 = u1
-                last_u2 = u2
-    
-            number_of_bids =len(agent['bids'])-1
-            agent['moves']['silent'] = agent['moves']['silent']/number_of_bids
-            agent['moves']['nice'] = agent['moves']['nice']/number_of_bids
-            agent['moves']['fortunate'] = agent['moves']['fortunate']/number_of_bids
-            agent['moves']['selfish'] = agent['moves']['selfish']/number_of_bids
-            agent['moves']['concession'] = agent['moves']['concession']/number_of_bids
-            agent['moves']['unfortunate'] = agent['moves']['unfortunate']/number_of_bids
-
-        def calc_session(session):
-            session_id = ''.join([i for i in session['id'] if not i.isdigit()])
-            session_data = session['data']
-            agent1_name, agent2_name = session_id.split('_')
-
-            agent1 = init_agent(agent1_name, session_data['Utility1'])
-            agent2 = init_agent(agent2_name, session_data['Utility2'])
-            agent1['bids'] = [x['agent1'] for x in session_data['bids'] if 'agent1' in x]
-            agent2['bids'] = [x['agent2'] for x in session_data['bids'] if 'agent2' in x]
-
-            update_moves(agent1, agent2)
-            update_moves(agent2, agent1)
-
-            parties[agent1['name']].append(agent1['moves'])
-            parties[agent2['name']].append(agent2['moves'])
-
+###################### LEARNING ###############################
         
-        def get_bid_util(agent_util, bid):
-            u = 0
-            fruit, juice, topping1, topping2 = bid.split(',')
-            u += agent_util['Fruit'][str(fruit)]*agent_util['Fruit']['weight']
-            u += agent_util['Juice'][str(juice)]*agent_util['Juice']['weight']
-            u += agent_util['Topping1'][str(topping1)]*agent_util['Topping1']['weight']
-            u += agent_util['Topping2'][str(topping2)]*agent_util['Topping2']['weight']
-            return u
-        
-        sessions = read_json_files()
-        for session in sessions:
-            calc_session(session)
-        for agent in parties:
-            tmp_moves = {}
-            for moves in parties[agent]:
-                tmp_moves = Counter(tmp_moves) + Counter(moves)
-            avg_moves = init_moves()
-            avg_moves.update(tmp_moves)
-            parties[agent] = {k: v/len(parties[agent]) for k,v in avg_moves.items()}
-        #pprint(parties)
 
+    def readingObservationMatrix(self,pathToRead):
+        parties = np.load(pathToRead).item()
+        #print(parties)
         for strat in parties:
             for move in parties[strat]:
                 #print(strat, move, parties[strat][move])
                 self.observationMatrix[self.getStateToIndex(strat),self.getObservationToIndex(move)] = parties[strat][move]
-        #print(self.observationMatrix)
-        
-
-
 
             
 ########################### NO MORE LEARNING ################################
@@ -400,7 +289,7 @@ class Hmm(AbstractHmm):
 
             # We update our beliefs
             filterAgent1 = self.filteringComputation(filterAgent1, evidence)
-            print('filter Agent1, round ',bidRound['round'],' : ',filterAgent1)
+            #print('filter Agent1, round ',bidRound['round'],' : ',filterAgent1)
 
              # We get the bid of agent 1
             bidAgent2 = bidRound['agent2']
@@ -409,10 +298,34 @@ class Hmm(AbstractHmm):
 
             # We update our beliefs
             filterAgent2 = self.filteringComputation(filterAgent2, evidence)
-            print('filter Agent2, round ',bidRound['round'],' : ',filterAgent2)
+            #print('filter Agent2, round ',bidRound['round'],' : ',filterAgent2)
             
+        return [filterAgent1, filterAgent2]
 
 
+    def filterResToDict(self,filterRes):
+        ''' Give a dict format of the result of filtering '''
+
+        result = {}
+        for i in range(len(filterRes)):
+            result[list(self.states.keys())[i]] = filterRes[i]
+        return result
+        
 myHmm = Hmm()
-myHmm.learningModel('training_logs')
-myHmm.filteringHmm('training_logs/conceder_random1.json')
+myHmm.readingObservationMatrix('obsMatrixTmp.npy')
+#[filtAgent1, filtAgent2] = myHmm.filteringHmm('../training_logs/conceder_random1.json')
+[filtAgent1, filtAgent2] = myHmm.filteringHmm(sys.argv[1])
+
+print('Probability of each strategy for Agent 1 :\n')
+
+dictAgent1 = myHmm.filterResToDict(filtAgent1)
+for strat in dictAgent1:
+    print(strat + " : " + str(dictAgent1[strat]))
+
+    
+print('\nProbability of each strategy for Agent 2 :\n')
+
+dictAgent2 = myHmm.filterResToDict(filtAgent2)
+for strat in dictAgent2:
+    print(strat + " : " + str(dictAgent2[strat]))
+    
